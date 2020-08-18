d356a5a3d803387548a9c0ec3d80b23f90d3e0e1
LAMAGNERE-1 <fre.lamagnere@gmail.com>
2020-08-18 14:26:45 +0200

    add author

--- perroquet/src/App.js
+++ perroquet/src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Author: Frederic
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
