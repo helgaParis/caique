e596b4523f564fd59a5064f6d9b6a9c29fb96e44
LAMAGNERE-1 <fre.lamagnere@gmail.com>
2020-08-26 15:43:17 +0200

    Build Jeudi 26

---
+++ perroquet/build/precache-manifest.fc94043aa017a4029bca34fda4654ac6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde385cf5514210588abb63c07fcee35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13cf461d7ff640df198e",
+    "url": "/static/css/main.60ada58b.chunk.css"
+  },
+  {
+    "revision": "36c361e5ef8e7a3aa436",
+    "url": "/static/js/2.333552a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.333552a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13cf461d7ff640df198e",
+    "url": "/static/js/main.8a26952a.chunk.js"
+  },
+  {
+    "revision": "5379cbcab736c5949b3c",
+    "url": "/static/js/runtime-main.00af78fc.js"
+  },
+  {
+    "revision": "06eb95da7155ee41f4fd72dc602ec9bc",
+    "url": "/static/media/chat1.06eb95da.jpg"
+  },
+  {
+    "revision": "f0f363ff898f7d093e07061773bd6540",
+    "url": "/static/media/chat2.f0f363ff.jpg"
+  },
+  {
+    "revision": "f0b102ed2782b6ffee53e176fc15a042",
+    "url": "/static/media/chat3.f0b102ed.jpg"
+  }
+]);
